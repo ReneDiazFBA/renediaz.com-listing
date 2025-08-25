7ea7dd1ad92d25df7b5b99f1c2098e29891bf6e1
Rene Diaz <rdiaz@renediaz.com>
2025-08-24 20:58:47 -0500

    1

--- keywords/app_keywords_deduplicado.py
+++ keywords/app_keywords_deduplicado.py
@@ -4,7 +4,7 @@ import streamlit as st
 import pandas as pd
 from typing import Optional
 from utils.nav_utils import render_subnav
-from keywords.funcional_keywords_maestro import build_master_raw
+from keywords.funcional_keywords_deduplicado import build_master_raw
 
 
 def mostrar_keywords_deduplicado(excel_data: Optional[pd.ExcelFile] = None):
